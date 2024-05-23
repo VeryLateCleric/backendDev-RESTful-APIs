aa3b875814cdbe3acefb0fe3c16647e6f42a4c48
Kenneth <kennethyoung.2960@gmail.com>
2024-05-23 04:33:47 -0700

    Added error and not-found handlers and 4 of 10 tests passing

--- RESTful-API/src/app.js
+++ RESTful-API/src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-
 const notes = require("./data/notes-data");
+
+app.use(express.json());
 
 app.get("/notes/:noteId", (req, res) => {
   const noteId = Number(req.params.noteId);
@@ -16,7 +17,15 @@ app.get("/notes", (req, res) => {
 // TODO: Add ability to create a new note
 
 // TODO: Add not-found handler
+app.use((req, res, next) => {
+  const error = new Error(`Not found: ${req.originalUrl}`);
+  error.status = 404;
+  next(error);
+});
 
 // TODO: Add error handler
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({ error: error.message });
+});
 
 module.exports = app;
